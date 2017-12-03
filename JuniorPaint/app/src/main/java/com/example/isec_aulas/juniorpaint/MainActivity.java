3ec4c31373a137e92515ab3bf75e43fcf38112ae
PedroLA <p.m.cruz@hotmail.com>
2017-12-03 22:13:06 +0000

    Start Project v0.1

---
+++ JuniorPaint/app/src/main/java/com/example/isec_aulas/juniorpaint/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.isec_aulas.juniorpaint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onDesenha(View v) {
+        Intent intent = new Intent(this,Desenha.class);
+        startActivity(intent);
+    }
+/*
+    public void onGaleria(View v) {
+        Intent intent = new Intent(this,GaleriaActivity.class);
+        startActivity(intent);
+    }
+    public void onCamera(View v) {
+        Intent intent = new Intent(this,CameraActivity.class);
+        startActivity(intent);
+    }
+    public void onHistorico(View v) {
+        Intent intent = new Intent(this,HistoricoActivity.class);
+        startActivity(intent);
+    }*/
+}
